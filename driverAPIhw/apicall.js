d3a932c6ca5b7ceb07f8566e70c70905f5316978
odonnellp11 <96839471+odonnellp11@users.noreply.github.com>
2022-04-13 18:07:33 -0400

    main

--- driverAPIhw/apicall.js
+++
@@ -1,8 +0,0 @@
-let getData = async () => {
-    let response = await axios.get('https://ergast.com/api/f1/2020/1/driverStandings');
-    if (response.status == 200){
-        return response.data
-    }
-    return'api call broken'
-}
-getData();
